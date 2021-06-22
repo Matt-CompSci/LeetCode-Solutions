730a3e312bfa96026fea00197424ba8fa49aecdd
Matt-CompSci <gmatt1923@gmail.com>
2021-06-22 15:41:37 +0100

    Update Length_of_longest_substring.py

--- Length_of_longest_substring.py
+++ Length_of_longest_substring.py
@@ -1,22 +1,14 @@
 # Calculate max length of substring with none repeating characters
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 1: return 1
-        if len(s) == 2: return s[0] != s[1] and 2 or 1
+        char_map = {}
+        substring_start = 0
+        max_substring_len = 0
         
-        charList = list(s)
-        maxSubstringLength = 0
-        currentSubstringLength = 0
-        
-        for idx, startChar in enumerate(charList):
-            currentCharList = []
-            currentSubstringLength = 0
-            for char in charList[idx:len(charList)]:
-                if char in currentCharList:
-                    if currentSubstringLength > maxSubstringLength:
-                        maxSubstringLength = currentSubstringLength
-                        currentSubstringLength = 0
-                else:
-                    currentSubstringLength += 1
-                    currentCharList.append(char)
-        return maxSubstringLength
+        for idx, char in enumerate(s):
+            if char in char_map.keys():
+                if char_map[char] >= substring_start:
+                    substring_start = char_map[char] + 1
+            max_substring_len = max(max_substring_len, idx - substring_start + 1)
+            char_map[char] = idx
+        return max_substring_len
